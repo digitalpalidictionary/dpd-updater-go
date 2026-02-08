68b11452b2330b4aca1d9f6d59678b7961991af6
bdhrs <bodhirasa@gmail.com>
2026-02-08 19:57:46 +0530

    app: handle error returns in main.go

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/digitalpalidictionary/dpd-updater-go/internal/config"
 	"github.com/digitalpalidictionary/dpd-updater-go/internal/ui"
 )
 
 func main() {
-	cm := config.NewConfigManager()
-	cfg := cm.LoadConfig()
+	cm, err := config.NewConfigManager()
+	if err != nil {
+		log.Fatalf("Failed to create config manager: %v", err)
+	}
+
+	cfg, err := cm.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
 
 	application := ui.NewUI(cfg, cm)
 	application.Start()
